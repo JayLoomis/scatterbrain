3a5437bdeeed834346f6638ebb9d7078e8466884
Jay Loomis <jay.loomis@gmail.com>
2016-03-06 16:14:29 -0800

    Project planning and documentation starts.

--- scatterbrain.py
+++ scatterbrain.py
@@ -24,16 +24,89 @@
 
 import datetime
 
+
+class Contact:
+    """
+    Structure-style class for holding contact information.
+    """
+
+    def __init__(self, last, first, email, discipline=None, role=None, phone=None):
+        self.__last_name = last
+        self.__first_name = first
+        self.__discipline = discipline
+        self.__role = role
+        self.__phone = phone
+        self.__email = email
+
+    @property
+    def last_name(self):
+        return self.__last_name
+
+    @last_name.setter
+    def last_name(self, name):
+        self.__last_name = name
+
+    @property
+    def first_name(self):
+        return self.__first_name
+
+    @first_name.setter
+    def first_name(self, name):
+        self.__first_name = name
+
+    @property
+    def discipline(self):
+        return self.__discipline
+
+    @discipline.setter
+    def discipline(self, contact_discipline):
+        self.__discipline = contact_discipline
+
+    @property
+    def role(self):
+        return self.__role
+
+    @role.setter
+    def role(self, contact_role):
+        self.__role = contact_role
+
+    @property
+    def phone(self):
+        return self.__phone
+
+    @phone.setter
+    def phone(self, phone_number):
+        self.__phone = phone_number
+
+    @property
+    def email(self):
+        return self.__email
+
+    @email.setter
+    def email(self, address):
+        self.__email = address
+
+
 class WorkItem:
     """
     The base class for tasks and projects.
     """
     def __init__(self):
+        self.__description = None
         self.__due_date = None
         self.__priority = -1
         self.__contacts = []
         self.__info_links = None
+        self.__tasks = None
 
+    @property
+    def description(self):
+        return self.__description
+
+    @description.setter
+    def description(self, desc):
+        assert type(desc) == str, 'Value passed as a WorkItem description is not a string.'
+        self.__description = desc
 
     @property
     def priority(self):
@@ -54,9 +127,20 @@ class WorkItem:
             raise ValueError('Due dates must be in the future.')
 
     def add_contact(self, contact):
-        # Validate the variable?
+        # Validate the argument?
 
         self.__contacts.append(contact)
+
+    def remove_contact(self, last, first):
+        for index, contact in enumerate(self.__contacts):
+            if contact.last_name == last and contact.first_name == first:
+                del self.__contacts[index]
+                return True
+
+        # We'll only get to this point if the name isn't found
+        return False
+
+
 
 
 
